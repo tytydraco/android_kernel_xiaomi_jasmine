9c7ded53cd3779abe8f934cf3fbed6dbc025c2c3
Connor O'Brien <connoro@google.com>
2017-05-19 12:36:17 -0700

    sched/tune: allow negative cpu boosts
    schedtune sets 0 as the floor for calculating CPU max boost, so
    negative schedtune.boost values do not affect CPU frequency
    decisions. Remove this restriction to allow userspace to apply
    negative boosts when appropriate.
    Also change treatment of the root boost group to match the other
    groups, so it only affects the CPU boost if it has runnable tasks on
    the CPU.
    
    Test: set all boosts negative; sched_boost_cpu trace events show
    negative CPU margins.
    
    Change-Id: I89f3470299aef96a18797c105f02ebc8f367b5e1
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -311,15 +311,13 @@ static void
 schedtune_cpu_update(int cpu, u64 now)
 {
 	struct boost_groups *bg;
-	int boost_max;
+	int boost_max = INT_MIN;
 	u64 boost_ts = now;
 	int idx;
 
 	bg = &per_cpu(cpu_boost_groups, cpu);
 
-	/* The root boost group is always active */
-	boost_max = bg->group[0].boost;
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
+	for (idx = 0; idx < BOOSTGROUPS_COUNT; ++idx) {
 		/*
 		 * A boost group affects a CPU only if it has
 		 * RUNNABLE tasks on that CPU or it has hold
@@ -335,10 +333,10 @@ schedtune_cpu_update(int cpu, u64 now)
 		boost_max = bg->group[idx].boost;
 		boost_ts =  bg->group[idx].ts;
 	}
-	/* Ensures boost_max is non-negative when all cgroup boost values
-	 * are neagtive. Avoids under-accounting of cpu capacity which may cause
-	 * task stacking and frequency spikes.*/
-	boost_max = max(boost_max, 0);
+
+	/* If there are no active boost groups on the CPU, set no boost  */
+	if (boost_max == INT_MIN)
+		boost_max = 0;
 	bg->boost_max = boost_max;
 	bg->boost_ts = boost_ts;
 }
