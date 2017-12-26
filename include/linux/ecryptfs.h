c831a2889992ee4d66b4bac35b1e02eb70e24119
Sultanxda <sultanxda@gmail.com>
2017-12-25 21:34:24 -0800

    treewide: Fix compile errors
    These are mostly from disabling debugfs.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/ecryptfs.h
+++ include/linux/ecryptfs.h
@@ -158,7 +158,7 @@ static inline int ecryptfs_register_to_events(
 	return 1; /* dummy handle */
 }
 
-static int ecryptfs_unregister_from_events(int user_handle)
+static inline int ecryptfs_unregister_from_events(int user_handle)
 {
 	return 0;
 }
