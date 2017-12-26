f7fe0be2138a86daf31f174810201452085c130f
Sultanxda <sultanxda@gmail.com>
2017-12-25 23:59:54 -0800

    wakeup: Expose wakeup_sources via procfs when debugfs isn't present
    Full path: /proc/wakelocks
    
    /proc/wakelocks is the alternative wakelocks path that the Android
    framework supports.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,6 +16,7 @@
 #include <linux/debugfs.h>
 #include <linux/pm_wakeirq.h>
 #include <linux/types.h>
+#include <linux/proc_fs.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -1099,6 +1100,10 @@ static int __init wakeup_sources_debugfs_init(void)
 {
 	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
 			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
+	/* Fall back to procfs if debugfs is not available */
+	if (wakeup_sources_stats_dentry == ERR_PTR(-ENODEV))
+		proc_create("wakelocks", S_IRUGO,
+					NULL, &wakeup_sources_stats_fops);
 	return 0;
 }
 
