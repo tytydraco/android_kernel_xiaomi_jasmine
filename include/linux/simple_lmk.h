ab23273fc48fa6be083c2ba0da4ab84cac292fd7
Sultan Alsawaf <sultanxda@gmail.com>
2018-08-28 12:20:37 -0700

    simple_lmk: Introduce simple low memory killer for Android
    This is a simple replacement for the Android Low Memory Killer driver that
    simply targets a predefined number of pages to be freed every time a memory
    allocation attempt is about to utilize direct reclaim. Processes are killed
    according to the priorities that Android gives them; in other words, the
    least important processes are always killed first.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

---
+++ include/linux/simple_lmk.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2018, Sultan Alsawaf <sultanxda@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _SIMPLE_LMK_H_
+#define _SIMPLE_LMK_H_
+
+void simple_lmk_mem_reclaim(void);
+
+#endif /* _SIMPLE_LMK_H_ */
