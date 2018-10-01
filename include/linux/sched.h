88bb59b3fdcabdd0a4ea6bf37f7732b3a079c0c1
Sultan Alsawaf <sultanxda@gmail.com>
2018-10-01 09:30:35 -0700

    simple_lmk: Introduce Simple LMK (Low Memory Killer) for Android
    This is a replacement for the Android Low Memory Killer driver that simply
    targets a predefined number of pages to be freed every time a memory
    allocation attempt is about to utilize direct reclaim, or whenever it seems
    like kswapd is having trouble freeing enough memory in a timely manner.
    Processes are killed according to the priorities that Android gives them;
    in other words, the least important processes are always killed first.
    
    One way that memory reclaim is triggered is in the page-alloc slow path
    after compaction has failed. When the Simple LMK memory reclaim process is
    entered, memory is reclaimed in an infinite loop until the requested memory
    allocation is fulfilled when pages are freed. During this process, Simple LMK
    ensures that memory reclaim is invoked at least once every
    CONFIG_ANDROID_SIMPLE_LMK_OOM_TIMEOUT milliseconds. OOM'd memory allocations
    are placed on a queue and are prioritized over non-OOM'd memory allocations
    when new pages are freed. This is to ensure that each OOM'd memory allocation
    gets fulfilled as quickly as possible.
    
    The other way that memory reclaim occurs is via kswapd. When kswapd runs
    for longer than CONFIG_ANDROID_SIMPLE_LMK_KSWAPD_TIMEOUT milliseconds
    without sleeping, then Simple LMK ensures that memory reclaim is invoked at
    least once every CONFIG_ANDROID_SIMPLE_LMK_KSWAPD_TIMEOUT milliseconds.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2010,6 +2010,11 @@ struct task_struct {
 	unsigned long	task_state_change;
 #endif
 	int pagefault_disabled;
+	atomic64_t *concurrent_active_time;
+	atomic64_t *concurrent_policy_time;
+#ifdef CONFIG_ANDROID_SIMPLE_LMK
+	bool lmk_sigkill_sent;
+#endif
 /* CPU-specific state of this task */
 	struct thread_struct thread;
 /*
