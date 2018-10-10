5b0312a253db49488308553bb9fc6a25de9aa642
Tyler Nijmeh <tylernij@gmail.com>
2018-10-09 19:38:36 -0700

    Revert "qcacld-3.0: Enable debugfs usage for wahoo"
    This reverts commit fa3efdd21f6f4d635f7e4d2d67067f5bba32e95c.

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_debugfs.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_debugfs.c
@@ -35,10 +35,6 @@
  */
 
 #ifdef WLAN_OPEN_SOURCE
-#if defined(CONFIG_ANDROID) && !defined(CONFIG_DEBUG_FS)
-#define CONFIG_DEBUG_FS
-#endif
-
 #include <wlan_hdd_includes.h>
 #include <wlan_hdd_debugfs.h>
 #include <wlan_hdd_wowl.h>
@@ -902,9 +898,6 @@ QDF_STATUS hdd_debugfs_init(hdd_adapter_t *adapter)
 
 	if (NULL == adapter->debugfs_phy)
 		return QDF_STATUS_E_FAILURE;
-
-	if (IS_ENABLED(CONFIG_BOARD_MUSKIE) || IS_ENABLED(CONFIG_BOARD_TAIMEN))
-		return wlan_hdd_init_power_stats_debugfs(adapter);
 
 	if (NULL == debugfs_create_file("wow_enable", 00400 | 00200,
 					adapter->debugfs_phy, adapter,
