88d5b9c3f0532877c2e1a9acec4ac79fcdf3b0c2
Tyler Nijmeh <tylernij@gmail.com>
2018-10-15 19:56:18 -0700

    HACK: qcom: remove unused osm function
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -2506,38 +2506,6 @@ fail:
 	return NULL;
 }
 
-static u64 clk_osm_get_cpu_cycle_counter(int cpu)
-{
-	struct clk_osm *c;
-	u32 val;
-	unsigned long flags;
-
-	if (logical_cpu_to_clk(cpu) == pwrcl_clk.hw.clk)
-		c = &pwrcl_clk;
-	else if (logical_cpu_to_clk(cpu) == perfcl_clk.hw.clk)
-		c = &perfcl_clk;
-	else {
-		pr_err("no clock device for CPU=%d\n", cpu);
-		return 0;
-	}
-
-	spin_lock_irqsave(&c->lock, flags);
-	val = clk_osm_read_reg_no_log(c, OSM_CYCLE_COUNTER_STATUS_REG);
-
-	if (val < c->prev_cycle_counter) {
-		/* Handle counter overflow */
-		c->total_cycle_counter += UINT_MAX -
-			c->prev_cycle_counter + val;
-		c->prev_cycle_counter = val;
-	} else {
-		c->total_cycle_counter += val - c->prev_cycle_counter;
-		c->prev_cycle_counter = val;
-	}
-	spin_unlock_irqrestore(&c->lock, flags);
-
-	return c->total_cycle_counter;
-}
-
 static void populate_opp_table(struct platform_device *pdev)
 {
 	int cpu;
