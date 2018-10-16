a9080e47c65d0eb63703817c54fbb95b931a870b
tytydraco <tylernij@gmail.com>
2018-10-16 10:27:51 -0700

    HACK: cpufreq: add Kconfig interface for iowait boost
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -705,7 +705,7 @@ static int sugov_init(struct cpufreq_policy *policy)
                 }
 	}
 
-	tunables->iowait_boost_enable = policy->iowait_boost_enable;
+	tunables->iowait_boost_enable = IS_ENABLED(CONFIG_IOWAIT_BOOST_ENABLE);
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
