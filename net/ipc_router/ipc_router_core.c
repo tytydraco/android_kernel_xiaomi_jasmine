dbcac6b9fbcc3ab33771846dd5f76cd8e23a13f5
Yaroslav Furman <yaro330@gmail.com>
2018-12-14 14:48:31 +0300

    net: ipc_router_core: fix memleak with IPC_LOGGING disabled
    CAF tried to fix it already but forgot to kfree the sub_log_ctx.
    Why bother even allocating it though?
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3965,6 +3965,7 @@ static void debugfs_init(void) {}
  */
 static void *ipc_router_create_log_ctx(char *name)
 {
+#ifdef CONFIG_IPC_LOGGING
 	struct ipc_rtr_log_ctx *sub_log_ctx;
 
 	sub_log_ctx = kmalloc(sizeof(struct ipc_rtr_log_ctx),
@@ -3984,6 +3985,9 @@ static void *ipc_router_create_log_ctx(char *name)
 	INIT_LIST_HEAD(&sub_log_ctx->list);
 	list_add_tail(&sub_log_ctx->list, &log_ctx_list);
 	return sub_log_ctx->log_ctx;
+#else
+	return NULL;
+#endif
 }
 
 static void ipc_router_log_ctx_init(void)
