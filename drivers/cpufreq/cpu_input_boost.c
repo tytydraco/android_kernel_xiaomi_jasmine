709215cf851679532f9e27367f4568435ae9d225
kdrag0n <dragon@khronodragon.com>
2019-02-05 20:53:13 -0800

    cpu_input_boost: Rework scheduling setup
    This patch minimizes the latency of an input boost by creating a
    dedicated kworker thread and scheduling it with the SCHED_FIFO realtime
    policy. We use an RT priority of 98 (max = 100) to have the input boost
    task preempt userspace RT tasks if needed while being preempted by
    in-kernel RT tasks which are set at priority 99 or higher. To reduce
    impact on the overall system performance due to the frequent preemption,
    we bind the kworker to only run on last 3 little cluster CPU cores. CPU
    0 is considered too busy, so leave the boosting workers off of that
    core.
    
    Also, since now the cpu_input_boost workqueue would only handle the work
    to disable an active input boost, we can remove the workqueue entirely
    and move the disabling work to the shared system-wide power efficient
    workqueue because it's not latency critical.
    
    Thanks to TheCrazyLex (Alex Naidis <alex.naidis@linux.com>) for the
    original version of this patch for the cpu-boost driver; I simply
    converted it over to cpu_input_boost.
    
    Sources used:
     - arter97/android_kernel_oneplus_sdm845@a2790f1
     - Razer Phone 2 "aura" kernel source
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,6 +11,7 @@
 #include <linux/input.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
+#include <linux/kthread.h>
 
 static __read_mostly unsigned short input_boost_duration = CONFIG_INPUT_BOOST_DURATION_MS;
 static __read_mostly int input_stune_boost = CONFIG_INPUT_BOOST_STUNE_LEVEL;
@@ -36,12 +37,13 @@ module_param(display_stune_boost, int, 0644);
 #define DISPLAY_STUNE_BOOST	BIT(8)
 
 struct boost_drv {
-	struct workqueue_struct *wq;
-	struct work_struct input_boost;
-	struct work_struct general_boost;
+	struct kthread_worker worker;
+	struct task_struct *worker_thread;
+	struct kthread_work input_boost;
+	struct kthread_work general_boost;
 	struct delayed_work input_unboost;
 	struct delayed_work general_unboost;
-	struct work_struct max_boost;
+	struct kthread_work max_boost;
 	struct delayed_work max_unboost;
 	struct notifier_block fb_notif;
 	atomic64_t max_boost_expires;
@@ -122,7 +124,7 @@ void cpu_input_boost_kick(void)
 	if (!b)
 		return;
 
-	queue_work(b->wq, &b->input_boost);
+	queue_kthread_work(&b->worker, &b->input_boost);
 }
 
 static void __cpu_input_boost_kick_max(struct boost_drv *b,
@@ -141,7 +143,7 @@ static void __cpu_input_boost_kick_max(struct boost_drv *b,
 		new_expires) != curr_expires);
 
 	atomic_set(&b->max_boost_dur, duration_ms);
-	queue_work(b->wq, &b->max_boost);
+	queue_kthread_work(&b->worker, &b->max_boost);
 }
 
 static void __cpu_general_boost_kick(struct boost_drv *b,
@@ -160,7 +162,7 @@ static void __cpu_general_boost_kick(struct boost_drv *b,
 		new_expires) != curr_expires);
 
 	atomic_set(&b->general_boost_dur, duration_ms);
-	queue_work(b->wq, &b->general_boost);
+	queue_kthread_work(&b->worker, &b->general_boost);
 }
 
 void cpu_general_boost_kick(unsigned int duration_ms)
@@ -196,7 +198,7 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
-static void input_boost_worker(struct work_struct *work)
+static void input_boost_worker(struct kthread_work *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
 	u32 state = get_boost_state(b);
@@ -208,11 +210,11 @@ static void input_boost_worker(struct work_struct *work)
 			&b->input_stune_slot);
 	}
 
-	queue_delayed_work(b->wq, &b->input_unboost,
+	queue_delayed_work(system_power_efficient_wq, &b->input_unboost,
 		msecs_to_jiffies(input_boost_duration));
 }
 
-static void general_boost_worker(struct work_struct *work)
+static void general_boost_worker(struct kthread_work *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), general_boost);
 	u32 state = get_boost_state(b);
@@ -224,7 +226,7 @@ static void general_boost_worker(struct work_struct *work)
 			&b->general_stune_slot);
 	}
 
-	queue_delayed_work(b->wq, &b->general_unboost,
+	queue_delayed_work(system_power_efficient_wq, &b->general_unboost,
 		msecs_to_jiffies(atomic_read(&b->general_boost_dur)));
 }
 
@@ -248,7 +250,7 @@ static void general_unboost_worker(struct work_struct *work)
 	clear_stune_boost(b, state, GENERAL_STUNE_BOOST, b->general_stune_slot);
 }
 
-static void max_boost_worker(struct work_struct *work)
+static void max_boost_worker(struct kthread_work *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), max_boost);
 	u32 state = get_boost_state(b);
@@ -260,7 +262,7 @@ static void max_boost_worker(struct work_struct *work)
 			&b->max_stune_slot);
 	}
 
-	queue_delayed_work(b->wq, &b->max_unboost,
+	queue_delayed_work(system_power_efficient_wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));	
 }
 
@@ -315,7 +317,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 		return;
 
 	atomic64_set(&b->prev_input_jiffies, jiffies);
-	queue_work(b->wq, &b->input_boost);
+	queue_kthread_work(&b->worker, &b->input_boost);
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
@@ -394,24 +396,43 @@ static struct input_handler cpu_input_boost_input_handler = {
 static int __init cpu_input_boost_init(void)
 {
 	struct boost_drv *b;
-	int ret;
+	int ret, i;
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 2 };
+	cpumask_t sys_bg_mask;
 
 	b = kzalloc(sizeof(*b), GFP_KERNEL);
 	if (!b)
 		return -ENOMEM;
 
-	b->wq = alloc_workqueue("cpu_input_boost_wq", WQ_HIGHPRI | WQ_UNBOUND, 0);
-	if (!b->wq) {
-		ret = -ENOMEM;
+	init_kthread_worker(&b->worker);
+	b->worker_thread = kthread_run(kthread_worker_fn, &b->worker,
+				       "cpu_input_boost_thread");
+	if (IS_ERR(b->worker_thread)) {
+		ret = PTR_ERR(b->worker_thread);
+		pr_err("Failed to start kworker, err: %d\n", ret);
 		goto free_b;
 	}
 
+	ret = sched_setscheduler(b->worker_thread, SCHED_FIFO, &param);
+	if (ret)
+		pr_err("Failed to set SCHED_FIFO on kworker, err: %d\n", ret);
+
+	/* Init the cpumask: 1-3 inclusive */
+	for (i = 1; i <= 3; i++)
+		cpumask_set_cpu(i, &sys_bg_mask);
+
+	/* Bind it to the cpumask */
+	kthread_bind_mask(b->worker_thread, &sys_bg_mask);
+
+	/* Wake it up */
+	wake_up_process(b->worker_thread);
+
 	atomic64_set(&b->max_boost_expires, 0);
-	INIT_WORK(&b->input_boost, input_boost_worker);
-	INIT_WORK(&b->general_boost, general_boost_worker);
+	init_kthread_work(&b->input_boost, input_boost_worker);
+	init_kthread_work(&b->general_boost, general_boost_worker);
 	INIT_DELAYED_WORK(&b->input_unboost, input_unboost_worker);
 	INIT_DELAYED_WORK(&b->general_unboost, general_unboost_worker);
-	INIT_WORK(&b->max_boost, max_boost_worker);
+	init_kthread_work(&b->max_boost, max_boost_worker);
 	INIT_DELAYED_WORK(&b->max_unboost, max_unboost_worker);
 	atomic_set(&b->state, 0);
 
