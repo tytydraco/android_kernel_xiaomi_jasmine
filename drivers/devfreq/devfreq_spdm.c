35c16df12947e1dde9873bc6bbbca257577c03a4
Tyler Nijmeh <tylernij@gmail.com>
2019-02-16 16:05:42 -0800

    treewide: Remove tracing and debug drivers
    These get linked in and we don't want them.
    
    gpu: msm: Omit tracing drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    iommu: Omit tracing drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    usb: host: Remove tracing drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    ARM: kernel: Remove trace drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: tytydraco <tylernij@gmail.com>
    
    lib: Remove tracing drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    net: wireless: Remove tracing drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    scsi: Remove debug drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    net: netfilter: Remove debug drivers
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
    treewide: Remove various debugging driver dependencies
    
    We really don't need all these to compile into the kernel when DEBUG_FS
    and DEBUG_KERNEL is off.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- drivers/devfreq/devfreq_spdm.c
+++ drivers/devfreq/devfreq_spdm.c
@@ -365,7 +365,9 @@ static int probe(struct platform_device *pdev)
 		goto no_spdm_device;
 	}
 
+#ifdef DEBUG_FS
 	spdm_init_debugfs(&pdev->dev);
+#endif
 	spdm_ipc_log_ctxt = ipc_log_context_create(SPDM_IPC_LOG_PAGES,
 							"devfreq_spdm", 0);
 
@@ -395,7 +397,9 @@ static int remove(struct platform_device *pdev)
 
 	data = platform_get_drvdata(pdev);
 
+#ifdef CONFIG_DEBUG_FS
 	spdm_remove_debugfs(data);
+#endif
 
 	if (data->devfreq)
 		devfreq_remove_device(data->devfreq);
--- drivers/media/usb/uvc/uvc_driver.c
+++ drivers/media/usb/uvc/uvc_driver.c
@@ -1845,7 +1845,9 @@ static void uvc_unregister_video(struct uvc_device *dev)
 
 		video_unregister_device(&stream->vdev);
 
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup_stream(stream);
+#endif
 	}
 
 	/* Decrement the stream count and call uvc_delete explicitly if there
@@ -1876,7 +1878,9 @@ static int uvc_register_video(struct uvc_device *dev,
 		return ret;
 	}
 
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init_stream(stream);
+#endif
 
 	/* Register the device with V4L. */
 
@@ -2689,11 +2693,15 @@ static int __init uvc_init(void)
 {
 	int ret;
 
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_init();
+#endif
 
 	ret = usb_register(&uvc_driver.driver);
 	if (ret < 0) {
+#ifdef CONFIG_DEBUG_FS
 		uvc_debugfs_cleanup();
+#endif
 		return ret;
 	}
 
@@ -2704,7 +2712,9 @@ static int __init uvc_init(void)
 static void __exit uvc_cleanup(void)
 {
 	usb_deregister(&uvc_driver.driver);
+#ifdef CONFIG_DEBUG_FS
 	uvc_debugfs_cleanup();
+#endif
 }
 
 module_init(uvc_init);
