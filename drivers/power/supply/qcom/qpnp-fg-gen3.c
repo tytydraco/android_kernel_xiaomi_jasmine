081aa78daf397930422b727a84398dcfa812da9d
Tyler Nijmeh <tylernij@gmail.com>
2019-05-13 19:25:23 -0700

    power: qpnp-fg-gen3: Fix msoc reporting charged too early
    Some batteries with wear will not reach the full 255 capacity. Scale
    this down to 240 to give around a 6% reporting threshold.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -759,7 +759,7 @@ static int fg_get_msoc_raw(struct fg_chip *chip, int *val)
 }
 
 #define FULL_CAPACITY	100
-#define FULL_SOC_RAW	255
+#define FULL_SOC_RAW	245
 static int fg_get_msoc(struct fg_chip *chip, int *msoc)
 {
 	int rc;
