5abc990c3aacc702ad36ddfe684d57de11b6a626
Tyler Nijmeh <tylernij@gmail.com>
2019-05-13 17:40:08 -0700

    power: smb-lib: Use 2.0A for DCP charging
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2485,7 +2485,7 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 
 #define SDP_CURRENT_UA			500000
 #define CDP_CURRENT_UA			1500000
-#define DCP_CURRENT_UA			1500000
+#define DCP_CURRENT_UA			2000000
 #define HVDCP_CURRENT_UA		3000000
 #define TYPEC_DEFAULT_CURRENT_UA	900000
 #define TYPEC_MEDIUM_CURRENT_UA		1500000
