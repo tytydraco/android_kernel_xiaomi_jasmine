64d8c61a78017d964503b1c7102bee98e2ccdfb0
Tyler Nijmeh <tylernij@gmail.com>
2019-05-29 18:49:22 -0700

    sched/tune: Add support for dynamic prefer_idle
    There are cases in which the kernel would benefit from adjusting the
    prefer_idle attribute from internal drivers. For example, a variety of
    CPU boosting drivers could utilize prefer_idle to increase performance
    and possibly energy efficiency.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3546,6 +3546,7 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
+int do_prefer_idle(char *st_name, u64 prefer_idle);
 #else /* !CONFIG_DYNAMIC_STUNE_BOOST */
 static inline int do_stune_boost(char *st_name, int boost, int *slot)
 {
@@ -3558,6 +3559,11 @@ static inline int do_stune_sched_boost(char *st_name, int *slot)
 }
 
 static inline int reset_stune_boost(char *st_name, int slot)
+{
+	return 0;
+}
+
+static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
 {
 	return 0;
 }
