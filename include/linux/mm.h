6c6866ca222e38c3adfcea7427e638e2808873de
Tyler Nijmeh <tylernij@gmail.com>
2019-06-30 12:57:29 -0700

    mm: Do readahead if requested size is over 64 KiB
    After we surpass 64 KiB, we could benefit from readahead. Anything less
    is less efficient.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2009,7 +2009,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	512	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MIN_READAHEAD	64	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
